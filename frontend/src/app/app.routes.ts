34513a67ba62300a3026574d75b5ceb058eb1b45
abdelalil544-blip <abdelalil544@gmail.com>
2026-02-05 15:01:45 +0100

    generate authebtification pages

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'auth',
+    pathMatch: 'full'
+  },
+  {
+    path: 'auth',
+    loadComponent: () => import('./pages/auth/auth.page').then((m) => m.AuthPage)
+  }
+];
